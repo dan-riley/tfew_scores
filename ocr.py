3329282f06ea1abc9c95eabb19f09cc6fce64eff
Dan Riley <dan.riley@colorado.edu>
2020-01-05 15:42:26 -0700

    Initial commit.  Command line processing.

---
+++ ocr.py
@@ -0,0 +1,174 @@
+import sys
+import re
+import cv2
+import pytesseract
+
+
+class Player(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.score = ''
+        self.order = 99
+        self.line = ''
+
+
+class OCRTF(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.imgs = []
+        self.output = ''
+        self.unmatched = ''
+        self.alliance = 'TFW2005'
+        self.count = 1
+        self.order = 0
+
+        if self.filename[-3:] == "mp4":
+            self.ocr_vid()
+        else:
+            self.imgs.append(cv2.imread(filename))
+
+        # Get the potential names
+        with open('names.txt', 'r') as f:
+            names = [line.rstrip() for line in f]
+
+        self.players = []
+        for name in names:
+            self.players.append(Player(name))
+
+    def getFrame(self, vid, sec):
+        vid.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+        success, img = vid.read()
+        if success:
+            self.imgs.append(img)
+            # cv2.imwrite("images/image" + str(len(self.imgs)) + ".jpg", img)
+
+        return success
+
+    def ocr_vid(self):
+        vid = cv2.VideoCapture(self.filename)
+
+        sec = 0
+        frameRate = 1
+        success = self.getFrame(vid, sec)
+        while success:
+            sec = sec + frameRate
+            sec = round(sec, 2)
+            success = self.getFrame(vid, sec)
+
+    def ocr_img(self, img):
+        # Crop image to read easier
+        height, width = img.shape[0:2]
+        img = img[0:height, 0:int(width / 2)]
+
+        # Invert image so text is black
+        img = cv2.bitwise_not(img)
+
+        # Save the processed image for review
+        # cv2.imwrite('processed/cvo' + str(self.count) + '.jpg', img)
+        self.count += 1
+
+        # Get the raw text
+        return pytesseract.image_to_string(img)
+
+    def processText(self, text):
+        unmatched = []
+        start = False
+        for line in text.splitlines():
+            # Only include the line if it has enough text to be useful
+            if start and len(line) > 5:
+                keep = True
+                # Check each rule but stop checking if we match one
+                if 'Soldier' in line:
+                    keep = False
+                elif 'Officer' in line:
+                    keep = False
+                elif 'Commander' in line:
+                    keep = False
+
+                added = False
+                # Add the line if none of the rules were hit
+                # Could make this a final elif, but this seems cleaner
+                if keep:
+                    for player in self.players:
+                        if player.name in line:
+                            rest = line.split(player.name)[1]
+                            scores = re.findall(r'\d+', rest)
+                            if scores:
+                                score = scores[-1]
+                                if int(score) % 5 or int(score) > 225:
+                                    score = ''
+                            else:
+                                score = ''
+
+                            if player.order == 99:
+                                self.order += 1
+                                player.score = score
+                                player.order = self.order
+                                player.line = line
+                                # print(str(player.order) + ' ' + player.name + ' ' + score);
+                            elif score and (player.score == '' or score > player.score):
+                                # print('Duplicate: ')
+                                # print(player.name + ' old: ' + player.score + ' new: ' + score)
+                                player.score = score
+                                player.line = line
+
+                            added = True
+                            break
+
+                    if not added:
+                        unmatched.append(line)
+
+            # Ignore everything up to the alliance name
+            if self.alliance in line:
+                start = True
+
+        # Put the lines back together
+        return '\n'.join(unmatched)
+
+    def processImages(self):
+        texts = []
+        for img in self.imgs:
+            text = self.ocr_img(img)
+            unmatched = self.processText(text)
+            if unmatched:
+                texts.append(unmatched)
+
+        self.unmatched = '\n'.join(texts)
+
+
+def main(filename):
+
+    ocr = OCRTF(filename)
+    ocr.processImages()
+
+    output = []
+    output.append('Order,Player,Score')
+    print('Data Review:\n')
+    ocr.players.sort(key=lambda x: x.order)
+    for player in ocr.players:
+        if player.order != 99:
+            print(player.name + ' ' + player.score)
+            orders = str(player.order)
+        else:
+            orders = ''
+
+        output.append(orders + ',' + player.name + ',' + player.score)
+
+    # output.append('\nData Entry:\n')
+    # ocr.players.sort(key=lambda x: x.name)
+    # for player in ocr.players:
+    #     output.append(player.name + ',' + player.score)
+
+    print("\n\nUnmatched:")
+    print(ocr.unmatched)
+    # output.append(ocr.unmatched)
+
+    fo = open('output.csv', 'w')
+    fo.write('\n'.join(output))
+    fo.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
